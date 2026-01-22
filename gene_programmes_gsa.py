d33bd4a38190046c7c1e3048e715db8606938b75
Yuankai He <yh464@cam.ac.uk>
2026-01-22 10:50:00 +0000

    bug fix

--- gene_programmes_gsa.py
+++ gene_programmes_gsa.py
@@ -8,7 +8,7 @@ cnmf_weights = pd.read_table(
     '/rds/project/rds-Nl99R8pHODQ/multiomics/programmes/cnmf/wang_2025/wang_2025_neocortex/' +
     f'wang_2025_neocortex.spectra.k_{cnmf_k}.dt_{cnmf_dt}.consensus.txt',index_col = 0).T
 cnmf_weights.columns = [f'F{i+1}' for i in range(cnmf_k)]
-replication_k = 14; replication_dt = '0_1'
+replication_k = 10; replication_dt = '0_1'
 replication_weights = pd.read_table(
     '/rds/project/rds-Nl99R8pHODQ/multiomics/programmes/cnmf/polioudakis_2019/neocx_wang_2025_genes/'+
     f'neocx_wang_2025_genes.spectra.k_{replication_k}.dt_{replication_dt}.consensus.txt',
